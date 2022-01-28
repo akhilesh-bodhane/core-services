8e2c7e50ae4c96f1569508251bcf2e2d3dfe0ae6
keshavpwc <92155184+keshavpwc@users.noreply.github.com>
2022-01-28 13:26:00 +0530

    Update CloudFileMgrUtils.java

--- egov-filestore/src/main/java/org/egov/filestore/repository/impl/CloudFileMgrUtils.java
+++ egov-filestore/src/main/java/org/egov/filestore/repository/impl/CloudFileMgrUtils.java
@@ -65,7 +65,7 @@ public class CloudFileMgrUtils {
 		Map<String, BufferedImage> mapOfImagesAndPaths = new HashMap<>();
 		try {
 			BufferedImage originalImage = ImageIO.read(file.getInputStream());
-			System.out.println("File input Stream : " + file.getInputStream());
+			System.out.println("File input Stream : " + file.getInputStream().toString());
 			if (null == originalImage) {
 				System.out.println("Original Image" + originalImage);
 				Map<String, String> map = new HashMap<>();
