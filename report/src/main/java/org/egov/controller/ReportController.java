b630402fa2d30ec72541e615f861b4f2fbf56ad1
keshavpwc <92155184+keshavpwc@users.noreply.github.com>
2022-03-11 18:34:52 +0530

    Update ReportController.java

--- report/src/main/java/org/egov/controller/ReportController.java
+++ report/src/main/java/org/egov/controller/ReportController.java
@@ -132,6 +132,43 @@ public class ReportController {
             throw new CustomException("ERROR_IN_RETRIEVING_REPORT_DATA", e.getMessage());
         }
     }
+	
+	
+    @PostMapping("/{moduleName}/Integrate/_get")
+    @ResponseBody
+    public ResponseEntity<?> getReportDatav2(@PathVariable("moduleName") String moduleName, @RequestBody @Valid final ReportRequest reportRequest,
+    final BindingResult errors ) {
+
+	//     @Valid @RequestBody ListReportDefination ListReportDefination ,
+	List<String> list=new ArrayList<String>();
+	List<ReportResponse> listt=new ArrayList<ReportResponse>();
+	// List<AAABCDFG> listReportDefination2 = ListReportDefination.getListReportDefination();
+	// List<ListReportDefination> listtt= ListReportDefination.getListReportDefination();
+	List<ReportResponse> reportResponse = new ArrayList<ReportResponse>();
+	//Adding elements in the List  
+	list.add("RPRentRegistryReport");  
+	list.add("OTRegistryReport");  
+	list.add("DCRegistryReport");  
+	list.add("RPDueAmountReport");
+
+	// ListReportDefination.getListReportDefination().
+	for (int i = 0; i < list.size(); i++) {
+
+	reportRequest.setReportName(list.get(i))  ;
+
+		try {
+		   reportResponse = reportService.getAllReportData(reportRequest, moduleName, reportRequest.getRequestInfo().getAuthToken());
+
+	//            return reportService.getReportDataSuccessResponse(reportResponse, reportRequest.getRequestInfo(), reportRequest.getTenantId());
+		} catch (Exception e) {
+		    log.error("Error in getting Report data ver1", e);
+		    throw new CustomException("ERROR_IN_RETRIEVING_REPORT_DATA", e.getMessage());
+		}
+		listt.addAll(reportResponse);
+	}
+	// reportResponse = reportService.getAllReportData1(reportRequest, moduleName, reportRequest.getRequestInfo().getAuthToken());
+	     return reportService.getReportDataSuccessResponse(listt, reportRequest.getRequestInfo(), reportRequest.getTenantId());
+    }	
 
 
     @PostMapping("{moduleName}/{version}/_reload")
