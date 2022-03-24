450ae70509818b635e354b53f9af36cc479bfde1
keshavpwc <keshav.k.verma@pwc.com>
2022-03-24 21:33:18 +0530

    axis response

--- egov-pg-service/src/main/java/org/egov/pg/service/gateways/axis/AxisGateway.java
+++ egov-pg-service/src/main/java/org/egov/pg/service/gateways/axis/AxisGateway.java
@@ -134,13 +134,13 @@ public class AxisGateway implements Gateway {
 			  
 			  if("paid".equals(order.get("status"))) {
 				  return Transaction.builder().txnId(currentStatus.getTxnId())
-							.txnAmount(order.get("amount_paid")).txnStatus(TxnStatusEnum.SUCCESS)
+							.txnAmount(order.get("amount_paid") + "").txnStatus(TxnStatusEnum.SUCCESS)
 							.gatewayTxnId(order.get("id")).gatewayPaymentMode(params.get("method"))
 							.gatewayStatusCode("")
 							.gatewayStatusMsg(params.get("description")).responseJson(mapToJson(params)).build();
 			  }else if("attempted".equals(order.get("status"))) {
 				  return Transaction.builder().txnId(currentStatus.getTxnId())
-							.txnAmount(order.get("amount_paid")).txnStatus(TxnStatusEnum.FAILURE)
+							.txnAmount(order.get("amount_paid") + "").txnStatus(TxnStatusEnum.FAILURE)
 							.gatewayTxnId(order.get("id")).gatewayPaymentMode(order.get("method"))
 							.gatewayStatusCode("")
 							.gatewayStatusMsg(params.get("error_description")).responseJson(mapToJson(params)).build();
