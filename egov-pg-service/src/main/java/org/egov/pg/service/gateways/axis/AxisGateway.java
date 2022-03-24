b0a0f782fd3db5da93646640b98c45b472f9f8c5
keshavpwc <keshav.k.verma@pwc.com>
2022-03-24 15:18:32 +0530

    amount log

--- egov-pg-service/src/main/java/org/egov/pg/service/gateways/axis/AxisGateway.java
+++ egov-pg-service/src/main/java/org/egov/pg/service/gateways/axis/AxisGateway.java
@@ -91,7 +91,7 @@ public class AxisGateway implements Gateway {
 			  System.out.println("Amount : " + transaction.getTxnAmount());
 			  JSONObject orderRequest = new JSONObject();
 				/* orderRequest.put(AMOUNT, Integer.valueOf(transaction.getTxnAmount())); */
-			  orderRequest.put(AMOUNT, 10);
+			  orderRequest.put(AMOUNT, 10000);
 			  orderRequest.put(CURRENCY_STR, CURRENCY);
 			  orderRequest.put(RECEIPT, transaction.getTxnId());
 			  Order order = razorpay.Orders.create(orderRequest);
@@ -100,7 +100,7 @@ public class AxisGateway implements Gateway {
 			  System.out.println("Order : " + order.toString());
 			  
 				/* responce.put(AMOUNT, Integer.valueOf(transaction.getTxnAmount())); */
-			  responce.put(AMOUNT, 10);
+			  responce.put(AMOUNT, 10000);
 			  responce.put(KEY, KEY_ID);
 			  responce.put(ORDER_ID, order.get("id"));
 			  responce.put(CALLBACK_URL, transaction.getCallbackUrl());
