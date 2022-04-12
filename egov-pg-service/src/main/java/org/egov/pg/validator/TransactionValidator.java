740b37204afc7856978e7b728d470ef0441c982f
keshavpwc <keshav.k.verma@pwc.com>
2022-04-12 20:44:48 +0530

    message changed

--- egov-pg-service/src/main/java/org/egov/pg/validator/TransactionValidator.java
+++ egov-pg-service/src/main/java/org/egov/pg/validator/TransactionValidator.java
@@ -216,7 +216,7 @@ public class TransactionValidator {
 				 * + (props.getEarlyReconcileJobRunInterval() * 2) + " mins");
 				 */
 				  errorMap.put("TXN_ABRUPTLY_DISCARDED",
-				  "A transaction for this bill has been abruptly discarded, please retry after 75 mins");
+				  "A transaction for this bill has been abruptly discarded, please retry after 60 mins");
 				
 				} 
 			if (curr.getTxnStatus().equals(Transaction.TxnStatusEnum.SUCCESS)) {
