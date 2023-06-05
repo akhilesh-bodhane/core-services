52545181ef50519a02886a5fe2413411e628eda0
keshavpwc <keshav.k.verma@pwc.com>
2023-06-05 13:07:21 +0530

    update txn

--- egov-pg-service/src/main/java/org/egov/pg/service/EnrichmentService.java
+++ egov-pg-service/src/main/java/org/egov/pg/service/EnrichmentService.java
@@ -94,7 +94,7 @@ public class EnrichmentService {
 				.lastModifiedTime(System.currentTimeMillis()).build();
 		newTxn.setAuditDetails(auditDetails);
 		
-		if(currentTxnStatus.getTxnStatus().toString().equals("PENDING")) {
+		if(currentTxnStatus.getTxnStatus().equals(Transaction.TxnStatusEnum.PENDING)) {
 			newTxn.setTxnStatus(Transaction.TxnStatusEnum.FAILURE);
 			System.out.println("New Txn Status : " + newTxn.getTxnStatus().toString());
 		} else {
