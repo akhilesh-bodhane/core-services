88c67c38e185fc26590bf879d4b2a1b094ee7449
keshavpwc <keshav.k.verma@pwc.com>
2025-05-15 01:12:22 +0530

    txn time increased by 30mins

--- egov-pg-service/src/main/java/org/egov/pg/repository/TransactionQueryBuilder.java
+++ egov-pg-service/src/main/java/org/egov/pg/repository/TransactionQueryBuilder.java
@@ -59,14 +59,17 @@ class TransactionQueryBuilder {
         StringBuilder builder = new StringBuilder(preparedQuery);
 
         //commented to fetch all PENDING queries
-		/*
-		 * if (!preparedQuery.contains("WHERE")) builder.append(" WHERE "); else
-		 * builder.append(" AND ");
-		 * 
-		 * builder.append(" pg.created_time >= ? "); preparedStmtList.add(startTime);
-		 * builder.append(" AND "); builder.append(" pg.created_time <= ? ");
-		 * preparedStmtList.add(endTime);
-		 */
+
+		if (!preparedQuery.contains("WHERE"))
+			builder.append(" WHERE ");
+		else
+			builder.append(" AND ");
+
+		builder.append(" pg.created_time >= ? ");
+		preparedStmtList.add(startTime);
+		builder.append(" AND ");
+		builder.append(" pg.created_time <= ? ");
+		preparedStmtList.add(endTime); 
 
         return builder.toString();
     }
